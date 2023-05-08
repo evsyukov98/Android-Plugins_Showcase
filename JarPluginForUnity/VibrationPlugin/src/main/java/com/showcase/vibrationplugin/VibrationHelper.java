cc722cfcbf2741348cfb9697cffcb02a9e2b304e
Ivan Evsyukov <evsyukov98@list.ru>
2023-05-09 03:45:28 +0400

    add VibrationHelper JavaObject class

--- JarPluginForUnity/VibrationPlugin/src/main/java/com/showcase/vibrationplugin/VibrationHelper.java
+++ JarPluginForUnity/VibrationPlugin/src/main/java/com/showcase/vibrationplugin/VibrationHelper.java
@@ -1,4 +1,21 @@
 package com.showcase.vibrationplugin;
 
+import android.content.Context;
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+
 public class VibrationHelper {
+
+    public static void vibrate(long milliseconds, Context context, int amplitude) {
+        Vibrator vibrateDefault = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        if (vibrateDefault != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                VibrationEffect vibrationEffect = VibrationEffect.createOneShot(milliseconds, amplitude);
+                vibrateDefault.vibrate(vibrationEffect);
+            } else {
+                vibrateDefault.vibrate(milliseconds);
+            }
+        }
+    }
 }
