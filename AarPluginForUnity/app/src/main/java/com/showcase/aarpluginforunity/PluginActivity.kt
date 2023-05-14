75985d744e8e3bb3aa4693230e84ed779904fa78
Ivan Evsyukov <evsyukov98@list.ru>
2023-05-14 22:21:46 +0400

    Add new Plugin AAR new activity opene

---
+++ AarPluginForUnity/app/src/main/java/com/showcase/aarpluginforunity/PluginActivity.kt
@@ -0,0 +1,16 @@
+package com.showcase.aarpluginforunity
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+
+class PluginActivity : Activity() {                          // Класс активити
+    override fun onCreate(savedInstanceState: Bundle?) {     // Стартовый класс активити
+        super.onCreate(savedInstanceState)                   // альтернатива base.
+
+        val textView = TextView(this)                 // Создаем экземпляр TextView
+        textView.setText(R.string.message)                   // Устанавливаем текст из R - Resources, string - strings.xml
+
+        setContentView(textView)                             // Отображаем TextView в активити
+    }
+}
