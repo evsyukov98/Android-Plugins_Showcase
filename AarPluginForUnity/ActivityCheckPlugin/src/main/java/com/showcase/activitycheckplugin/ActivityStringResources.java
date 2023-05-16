e7a04532cd1e286c21817c856574de4145908259
Ivan Evsyukov <evsyukov98@list.ru>
2023-05-17 01:33:11 +0400

    Add xml simple UI

--- AarPluginForUnity/ActivityCheckPlugin/src/main/java/com/showcase/activitycheckplugin/ActivityStringResources.java
+++ AarPluginForUnity/ActivityCheckPlugin/src/main/java/com/showcase/activitycheckplugin/ActivityStringResources.java
@@ -2,6 +2,8 @@ package com.showcase.activitycheckplugin;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ActivityStringResources extends Activity {
@@ -9,9 +11,24 @@ public class ActivityStringResources extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        TextView textView = new TextView(this);
+        // Задаем макет для этой активити. Макет определяет структуру пользовательского интерфейса активити
+        setContentView(R.layout.activity_string_resources);
+
+        // Ищем TextView в макете по его ID. Это тот же ID, который мы указали в макете XML
+        TextView textView = (TextView) findViewById(R.id.text_view);
+
+        // Устанавливаем текст для TextView. R.string.message это ссылка на строковый ресурс в файле ресурсов
         textView.setText(R.string.message);
 
-        setContentView(textView);
+        // Ищем Button в макете по его ID. Это тот же ID, который мы указали в макете XML
+        Button closeButton = (Button) findViewById(R.id.close_button);
+
+        // Устанавливаем ивент. Когда пользователь нажмет кнопку, будет вызван этот метод
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Закрываем активити. Это означает, что активити будет удалена из стека активити и пользователь вернется к предыдущей активити
+                finish();
+            }
+        });
     }
 }
